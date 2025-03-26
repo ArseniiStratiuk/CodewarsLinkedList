9a72797f98e9e36cf34d6dd04a4063bc5836a47d
Arsenii Stratiuk <stratuik.777@gmail.com>
2025-03-26 11:02:15 +0200

    Implement stringify function to convert a linked list to a string, using string concatenation and loops

--- Convert_a_linked_list_to_a_string/solution.py
+++ Convert_a_linked_list_to_a_string/solution.py
@@ -1,2 +1,12 @@
-# Solution for Convert_a_linked_list_to_a_string
+"""Solution for Convert a linked list to a string"""
 
+
+def stringify(node):
+    """Convert a linked list to a string"""
+    result = ''
+    current = node
+    while current:
+        result += f'{current.data} -> '
+        current = current.next
+    result += 'None'
+    return result
