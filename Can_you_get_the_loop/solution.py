58400cae034c6a2493d0fa6109f1751c9799a5f2
Arsenii Stratiuk <stratuik.777@gmail.com>
2025-04-04 17:33:25 +0300

    Implement loop_size function to calculate the size of a loop in a linked list

--- Can_you_get_the_loop/solution.py
+++ Can_you_get_the_loop/solution.py
@@ -1,2 +1,19 @@
 # Solution for Can_you_get_the_loop
 
+def loop_size(node):
+    slow = node
+    fast = node
+
+    while True:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+
+    count = 1
+    current = slow.next
+    while current != slow:
+        current = current.next
+        count += 1
+
+    return count
