1fcc6dc0b59a9820c947d43d6c85da09d3b1d6cd
Arsenii Stratiuk <stratuik.777@gmail.com>
2025-04-04 16:43:54 +0300

    Implement recursive reverse function for linked list and define Node class

--- Linked_Lists_Recursive_Reverse/solution.py
+++ Linked_Lists_Recursive_Reverse/solution.py
@@ -1,2 +1,13 @@
-# Solution for Linked_Lists_Recursive_Reverse
+class Node:
+    def __init__(self, data: int = None):
+        self.data = data
+        self.next = None
 
+def reverse(head: Node | None) -> Node | None:
+    def _reverse(node: Node | None, prev: Node | None = None) -> Node | None:
+        if not node:
+            return prev
+        next_node = node.next
+        node.next = prev
+        return _reverse(next_node, node)
+    return _reverse(head)
