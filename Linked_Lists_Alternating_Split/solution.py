43ad9d5f19ae3937ce7ef2ffb1766166f362552f
Arsenii Stratiuk <stratuik.777@gmail.com>
2025-04-04 17:04:16 +0300

    Implement alternating_split function to split a linked list into two alternating sublists and define Node and Context classes

--- Linked_Lists_Alternating_Split/solution.py
+++ Linked_Lists_Alternating_Split/solution.py
@@ -1,2 +1,38 @@
 # Solution for Linked_Lists_Alternating_Split
 
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+def alternating_split(head):
+    if not head or not head.next:
+        raise ValueError("List must contain at least two nodes")
+
+    first_head = head
+    second_head = head.next
+
+    first = first_head
+    second = second_head
+    current = head.next.next
+    toggle = True
+
+    while current:
+        if toggle:
+            first.next = current
+            first = first.next
+        else:
+            second.next = current
+            second = second.next
+        current = current.next
+        toggle = not toggle
+
+    first.next = None
+    second.next = None
+
+    return Context(first_head, second_head)
