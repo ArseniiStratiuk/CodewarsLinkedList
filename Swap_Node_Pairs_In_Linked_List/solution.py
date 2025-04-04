cf02fec56430b1218cbe763e7fd622f5001a1f34
Arsenii Stratiuk <stratuik.777@gmail.com>
2025-04-04 17:01:16 +0300

    Implement swap_pairs function to swap adjacent nodes in a linked list and define Node class

--- Swap_Node_Pairs_In_Linked_List/solution.py
+++ Swap_Node_Pairs_In_Linked_List/solution.py
@@ -1,2 +1,25 @@
 # Solution for Swap_Node_Pairs_In_Linked_List
 
+class Node:
+    """Node class for a linked list"""
+    def __init__(self, data = None, next_=None):
+        self.data = data
+        self.next = next_
+
+def swap_pairs(head: Node):
+    dummy = Node()
+    dummy.next = head
+    prev = dummy
+
+    while head and head.next:
+        first = head
+        second = head.next
+
+        prev.next = second
+        first.next = second.next
+        second.next = first
+
+        prev = first
+        head = first.next
+
+    return dummy.next
