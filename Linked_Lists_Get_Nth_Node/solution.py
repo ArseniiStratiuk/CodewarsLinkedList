98b131ec8de421406af675d36afe354c4f9952da
Arsenii Stratiuk <stratuik.777@gmail.com>
2025-04-04 11:28:34 +0300

    Implement get_nth function to retrieve the nth node from a linked list and add tests passed image

--- Linked_Lists_Get_Nth_Node/solution.py
+++ Linked_Lists_Get_Nth_Node/solution.py
@@ -1,2 +1,19 @@
 # Solution for Linked_Lists_Get_Nth_Node
 
+from preloaded import Node
+
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+    
+def get_nth(node, index):
+    count = 0
+    while True:
+        if node is None:
+            raise Exception
+        if count == index:
+            return node
+        node = node.next
+        count +=1
